dc8394abe51b24bb4befd96c99e7e213f8871f67
Tomasz-Budny <s208246@sggw.edu.pl>
2022-11-27 14:27:16 +0100

    Update Browse method and update browse view

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -19,7 +19,9 @@ namespace MvcMusicStore.Controllers
 
         public ActionResult Browse(string genre)
         {
-            var genreModel = new Genre { Name = genre };
+            var genreModel = storeDB.Genres.Include("Albums")
+                .Single(g => g.Name == genre);
+            
             return View(genreModel);
         }
         
