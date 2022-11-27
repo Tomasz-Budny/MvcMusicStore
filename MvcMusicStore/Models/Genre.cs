b9b4bd4d6befc69f06f565a047c949e1663987cc
Tomasz-Budny <s208246@sggw.edu.pl>
2022-11-27 13:42:48 +0100

    Update genre model

--- MvcMusicStore/Models/Genre.cs
+++ MvcMusicStore/Models/Genre.cs
@@ -1,7 +1,12 @@
-﻿namespace MvcMusicStore.Models
+﻿using System.Collections.Generic;
+
+namespace MvcMusicStore.Models
 {
     public class Genre
     {
-        public string Name { get; set; }
+        public int      GenreId     { get; set; }
+        public string   Name        { get; set; }
+        public string   Description { get; set; }
+        public List<Album> Albums   { get; set; }
     }
 }
