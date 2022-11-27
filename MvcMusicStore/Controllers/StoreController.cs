051747e3b00fc2a0e66816abe956eb2e8a83adea
Tomasz-Budny <s208246@sggw.edu.pl>
2022-11-27 14:36:47 +0100

    Update implementation of method details of StoreController.cs and update browse view

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -27,7 +27,8 @@ namespace MvcMusicStore.Controllers
         
         public ActionResult Details(int id)
         {
-            var album = new Album { Title = "Album " + id };
+            var album = storeDB.Albums.Find(id);
+            
             return View(album);
         }
     }
